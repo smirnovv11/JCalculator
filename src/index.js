9401169766a0335449d5efe5efa54145d2de27ef
smirnoff404 <smirnof528@gmail.com>
2024-09-21 20:34:15 +0300

    facts-added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import './style.css'
 import { onLoadTheme, onSwitchTheme } from './modules/theme'
 import { onCalcButtonClick, onKeyUp } from './modules/calculator'
+import { randomFact } from './modules/facts'
 
 const themeButton = document.querySelector('.theme-btn')
 const calcButtons = document.querySelectorAll('.calculator-btn')
 const body = document.querySelector('body')
+const jfact = document.querySelector('.jfact')
 
 onLoadTheme()
 
 themeButton.addEventListener('click', event => onSwitchTheme(event))
 calcButtons.forEach(el => el.addEventListener('click', event => onCalcButtonClick(event)))
 body.addEventListener('keyup', event => onKeyUp(event))
+jfact.innerHTML = 'JFact: ' + randomFact()
